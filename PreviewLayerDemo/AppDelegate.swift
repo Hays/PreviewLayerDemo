b06f18f7f3a64fbaea5177149fe7be5e27d9e683
Hays <huangwenxi@cvte.com>
2018-07-12 10:27:16 +0800

    [feature]add project

---
+++ PreviewLayerDemo/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PreviewLayerDemo
+//
+//  Created by 黄文希 on 2018/7/12.
+//  Copyright © 2018 Hays. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PreviewLayerDemo/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PreviewLayerDemo
+//
+//  Created by 黄文希 on 2018/7/12.
+//  Copyright © 2018 Hays. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
